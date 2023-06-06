a778b6521e5bdcb82aee75a3ae249b6fba4499fe
designwithashik <designwithashik@gmail.com>
2023-06-06 23:11:25 +0600

    Initial Commit with dark mode toggle added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react'
-import { themeChange } from 'theme-change'
 const Home = () => {
     const [theme, setTheme] = useState(
         localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
       );
     
-      // update state on toggle
       const handleToggle = (e) => {
         if (e.target.checked) {
           setTheme("dark");
@@ -14,11 +12,9 @@ const Home = () => {
         }
       };
     
-      // set theme state in localstorage on mount & also update localstorage on state change
       useEffect(() => {
         localStorage.setItem("theme", theme);
         const localTheme = localStorage.getItem("theme");
-        // add custom data-theme attribute to html tag required to update theme using DaisyUI
         document.querySelector("html").setAttribute("data-theme", localTheme);
       }, [theme]);
     
@@ -30,12 +26,9 @@ const Home = () => {
             <input
               type="checkbox"
               onChange={handleToggle}
-              // show toggle image based on localstorage theme
               checked={theme === "light" ? false : true}
             />
-            {/* light theme sun image */}
             <img src={'https://solarsystem.nasa.gov/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBamRTIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--8f58db6031cb325cfbaf366a330cf78148c0444a/Sun.png?disposition=inline'} alt="light" className="w-8 h-8 swap-on" />
-            {/* dark theme moon image */}
             <img src={'https://media.istockphoto.com/id/1334613123/vector/moon-and-star-black-icon-of-moon-for-night-pictogram-of-crescent-and-star-logo-for-sleep-and.jpg?s=612x612&w=0&k=20&c=Gy6DGTiwY2lsGPXRr2f7kqPELZEUtG1MsSEce1BkPdo='} alt="dark" className="w-8 h-8 swap-off" />
           </label>
         </button>

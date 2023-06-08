d83d64a6b3aee33e2c8fbcdbbc5dda584f000900
designwithashik <designwithashik@gmail.com>
2023-06-08 20:01:25 +0600

    Classes sorted in classes page

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -3,12 +3,30 @@ import useClasses from "../../hooks/useClasses";
 
 const Classes = () => {
     const [allClasses] = useClasses()
-            console.log(allClasses)
+    console.log(allClasses)
     return (
-        
+
         <div>
             <PageTitle>Classes</PageTitle>
-            
+
+            <div className="flex justify-center items-center flex-wrap mx-auto gap-5">
+                {allClasses.map(cls => {
+                    const {image, name,instructor, _id}= cls
+                    return ( <div key={_id} className="card w-96 h-72 image-full">
+                    <figure><img src={image} alt="class" className="object-cover w-96 h-72" /></figure>
+                    <div className="card-body">
+                            <h2 className="card-title">{name}!</h2>
+                            <p>{instructor}</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                )
+            })}
+            </div>
+
+
         </div>
     );
 };
--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -5,7 +5,16 @@ const Instructors = () => {
     console.log(allInstructors)
     return (
         <div>
-            
+            {/* TODO: Hover effect */}
+            <div className="flex justify-center items-center flex-wrap mx-auto gap-0">
+            {allInstructors.map(instructor => {
+                const {image, name, _id} = instructor
+                return (<div key={_id}>
+                    <img className="w-64 h-72 object-cover" src={image} alt="instructor" />
+                    <h2>{name}</h2>
+                </div>)
+            })}
+            </div>
         </div>
     );
 };
